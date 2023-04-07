eed1e86a4f0680e3e9ecbab2282d792c4c0d13a1
Maheshwar <S559173@nwmissouri.edu>
2023-04-07 12:53:31 -0500

    modified route for selector

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customerRouter = require('./routes/Customer');
 var boardRouter = require('./routes/board');
-var selectorRouter = require('./routes/Selector');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
