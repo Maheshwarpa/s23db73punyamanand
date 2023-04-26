226d910250c7925bc58bf408eb2e51f98575aa05
Maheshwar <S559173@nwmissouri.edu>
2023-04-26 11:01:58 -0500

    updated the files

--- models/customer.js
+++ models/customer.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose")
 const customerSchema = mongoose.Schema({
-    Cust_Name: String,
-    Cust_Age: Number,
-    Mail_Id: String
+    Cust_Name:String,
+    Cust_Age: {
+    type:Number,
+    min:1,
+    max:100
+    },
+    Mail_Id:String,
+    match:/^[a-zA-Z]+$/
 })
 module.exports = mongoose.model("customer",customerSchema)
 
--- routes/Customer.js
+++ routes/Customer.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var customer_controller = require('../controllers/customer');
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', customer_controller.customer_view_all_Page)
 router.get('/detail', customer_controller.customer_view_one_Page);
@@ -9,12 +17,6 @@ router.get('/detail', customer_controller.customer_view_one_Page);
 router.get('/create', customer_controller.customer_create_Page);
 /* GET delete costume page */
 router.get('/delete', customer_controller.customer_delete_Page);
-const secured = (req, res, next) => {
-    if (req.user){
-    return next();
-    }
-    req.session.returnTo = req.originalUrl;
-    res.redirect("/login");
-    }
-router.get('/update',secured,customer_controller.customer_update_Page);
+router.get('/delete', secured, customer_controller.customer_delete_Page);
+router.get('/update', secured, customer_controller.customer_update_Page);
 module.exports = router;
